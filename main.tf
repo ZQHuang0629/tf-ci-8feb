ae7114cf3beaa7d88f3fda76f05971c1d64e5bf8
zqhuang0629 <zhangqinqin0629@gmail.com>
2024-02-08 20:28:14 +0800

    edited s3 bucket v2

--- main.tf
+++ main.tf
@@ -1,6 +1,22 @@
+# resource "aws_s3_bucket" "example" {
+#     region = var.region
+#     bucket = "zhangqin-terraform-ci-bucket"
+#     force_destroy = true
+#     acl = "public-read"
+# }
+
 resource "aws_s3_bucket" "example" {
-    region = var.region
-    bucket = "zhangqin-terraform-ci-bucket"
-    force_destroy = true
-    acl = "public-read"
+  bucket = "zhangqin-terraform-ci-bucket"
+}
+
+resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.example.id
+  acl    = "public-read"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.example.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
