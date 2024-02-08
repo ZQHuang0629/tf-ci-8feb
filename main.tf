7a54a6e825c20a20ec22caabc8f5ddbf9dd69c73
zqhuang0629 <zhangqinqin0629@gmail.com>
2024-02-08 20:41:03 +0800

    edited s3 bucket v4

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-# resource "aws_s3_bucket" "example" {
-#     region = var.region
-#     bucket = "zhangqin-terraform-ci-bucket"
-#     force_destroy = true
-#     acl = "public-read"
-# }
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 resource "aws_s3_bucket" "example" {
   bucket = "zhangqin-terraform-ci-bucket"
 }
